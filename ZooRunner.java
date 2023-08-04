c2d715a534ef471031a0c07c97dd4d906904a2b8
Josiah Hunsinger <fencepost13302@gmail.com>
2023-08-03 22:51:23 -0400

    Initial commit

---
+++ ZooRunner.java
@@ -0,0 +1,8 @@
+import com.zoo.Zoo;
+
+public class ZooRunner {
+    public static void main(String[] args) {
+        Zoo myZoo = new Zoo("Zoo of Tears");
+        System.out.println( "Zoo name is \"" + myZoo.getName() + "\"" );
+    }
+}
---
+++ com/zoo/Zoo.java
@@ -0,0 +1,15 @@
+package com.zoo;
+
+public class Zoo {
+    String name;
+    
+    public Zoo(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+
